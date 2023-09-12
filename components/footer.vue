f534918b64d72b8c3933353fdf428367216f10c9
Kdnsdev <demo@krystaldigitalng.com>
2023-09-12 14:35:01 +0100

    finished the footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,41 @@
 <template>
-    <div class="bg-purple min-h-screen object-cover object-center bg-no-repeat">
-
+    <div class="pt-20">
+        <div class="flex px-20 flex-row justify-between w-full">
+            <div class="flex flex-col space-y-7 text-white">
+                <h2 class="text-2xl font-bold tracking-wider">FOLLOW US</h2>
+                <p class="text-lg">Lorem ipsum dolor sit amet, consectetur</p>
+                <div class="flex flex-row space-x-7 text-5xl">
+                    <NuxtLink to="/" class="hover:opacity-40"><i class='bx bxl-facebook-circle'></i></NuxtLink>
+                    <NuxtLink to="/" class="hover:opacity-40"><i class='bx bxl-twitter'></i></NuxtLink>
+                    <NuxtLink to="/" class="hover:opacity-40"><i class='bx bxl-instagram'></i></NuxtLink>
+                </div>
+            </div>
+            <div class="flex flex-col space-y-3 mr-28">
+                <h2 class="text-white text-2xl font-bold tracking-wider">SUBSCRIBE TO OUR NEWSLETTER</h2>
+                <p class="text-white text-lg">Thanks for contacting us! We will be in touch with you shortly.</p>
+            </div>
+        </div>
+        <hr class="w-full mt-20  opacity-30">
+        <div class="flex flex-row px-20 w-full justify-end mt-20">
+            <div class="flex flex-col space-y-7 mr-36">
+                <h2 class="text-white text-2xl font-bold">LEARN</h2>
+                <div class="flex flex-col space-y-1">
+                    <NuxtLink to="/" class="text-white text-lg hover:opacity-40">Cryptocurrency</NuxtLink>
+                    <NuxtLink to="/" class="text-white text-lg hover:opacity-40">Programming</NuxtLink>
+                    <NuxtLink to="/" class="text-white text-lg hover:opacity-40">Technologies</NuxtLink>
+                </div>
+            </div>
+            <div class="flex flex-col space-y-7">
+                <h2 class="text-white text-2xl font-bold">DEALS</h2>
+                <div class="flex flex-col space-y-1">
+                    <NuxtLink to="/" class="text-white text-lg hover:opacity-40">IdeaFirm Hosting Coupon</NuxtLink>
+                    <NuxtLink to="/" class="text-white text-lg hover:opacity-40">Pretended Company Discount Code</NuxtLink>
+                    <NuxtLink to="/" class="text-white text-lg hover:opacity-40">LogoIpsum Coupon Code</NuxtLink>
+                </div>
+            </div>
+        </div>
+        <hr class="w-full mt-20  opacity-30">
+        <p class="text-white opacity-40 tracking-wider text-sm py-10 px-20">Copyright © 2023 Tech Blogger | Powered by Tech
+            Blogger</p>
     </div>
 </template>
